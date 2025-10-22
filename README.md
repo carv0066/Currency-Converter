345931b607c808d042f8bd56f6acb7700bd6d9b3
Carlos Carvajal <carv0066@algonquinlive.com>
2025-10-21 23:30:32 -0400

    updating html and read.me

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-#Currency Converter
+# Currency Converter
+
+## Features
+
+- Convert between multiple currencies
+
+- Real-time exchange rates using an external API
+
+- Instant results after input
+
+- Responsive design for desktop
+
+- Simple and intuitive user interface
--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const convertButton = document.querySelector(".convert");
 convertButton.addEventListener("click", () => {
     console.log("convert Button");
 })
+
+
+//Connect currency converter API into app
+//The input text value should first only be nummbers
+//one list item should connect to one currency
+//second list item should be the currency it wants to be converted into
+//It updated when convert button gets clicked
