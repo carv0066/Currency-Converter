e9d320767ec71d9da39f00cfab6ace047004aae3
Carlos Carvajal <carv0066@algonquinlive.com>
2025-12-01 20:15:38 -0500

    fetched data from API

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
+import express from "express";
+import cors from "cors";
 import Currencyapi from '@everapi/currencyapi-js';
 import dotenv from 'dotenv';
+const app = express();
+const port = 3000;
+
+app.use(cors());
 
 // Load environment variables
 dotenv.config();
 
+//GET REQUEST
+//Grabbed value from API and log response
 const client = new Currencyapi(process.env.CURRENCY_API_KEY);
-client.historical({
-    date: '01-01-2021'
-}).then(response => {
-    console.log(response)
+
+app.get("/api/data", async (req, res) => {
+    console.log("Frontend requested data!");
+
+    try {
+        const latest = await client.latest();
+        res.json(latest);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: "failed to fetch latest currencies"});
+    }
+
+})
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+//Checking status on the amount of monthly uses i have left
+// client.status().then(response => {
+//     console.log("status", response);
+// });
+
+
+// in the select currency section I need to make it available to be chosen in the front end
--- src/script.js
+++ src/script.js
@@ -1,12 +1,27 @@
-const convertButton = document.querySelector(".convert");
+const selectElement = document.getElementById("currency-one");
+const currencyOptionsCollection = selectElement.options;
+const CurrencyOptions = Array.from(currencyOptionsCollection);
+
 //fetch from the api
+// "http://localhost:3000/api/data"
+fetch("http://localhost:3000/api/data")
+    .then(response => response.json()) 
+    .then(data => {
+        console.log(data);
+        const currencyArray = Object.entries(data.data);
 
-convertButton.addEventListener("click", () => {
-    console.log("convert Button has been clickedS");
-})
+        currencyArray.map(currency => {
+            console.log("currency", currency[0]);//get code for currency name
+            //Get each currency into one option for the list of options
+        })
+    
+    })
+    .catch(error => console.error('Error fetching data:', error));
 
 
-//Connect currency converter API into app
+//Connect API data to currency 1 selection
+//I need: Code and value
+//Loop trough the data.data array, inside each array grab the name and log it
 //The input text value should first only be nummbers
 //one list item should connect to one currency
 //second list item should be the currency it wants to be converted into
