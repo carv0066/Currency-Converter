4a6bc509afa166e23db21f7b6ed531c15aa70c4c
Carlos Carvajal <carv0066@algonquinlive.com>
2025-12-03 19:49:37 -0500

    Worked on amount input

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+const amount = document.getElementById("amount");
+
 const currencyOneList = document.getElementById("currency-one");
 const currencyOneOptionsCollection = currencyOneList.options;
 const currencyOneOptions = Array.from(currencyOneOptionsCollection);
@@ -5,9 +7,6 @@ const currencyOneOptions = Array.from(currencyOneOptionsCollection);
 const currencyTwoList = document.getElementById("currency-two");
 const currencyTwoOptionsCollection = currencyTwoList.options;
 const currencyTwoOptions = Array.from(currencyTwoOptionsCollection);
-
-console.log("Currency One", currencyOneOptions);
-console.log("Currency Two", currencyTwoOptions);
 
 //fetch from the api
 fetch("http://localhost:3000/api/data")
@@ -17,47 +16,80 @@ fetch("http://localhost:3000/api/data")
         const currencyArray = Object.entries(data.data); //transform object into array
         let currencyValueOne = 0;
         let currencyValueTwo = 0;
+        let currencyNameOne = "";
+        let currencyNameTwo = "";
 
         currencyArray.map(currency => {
             console.log("currency", currency); //get code for currency name
 
             //Get each currency into one option for the list of options
-            currencyOneOptions.forEach((optionOne) => {
+            currencyOneOptions.forEach(() => {
                 let createOption = document.createElement("option");
-                let currencyNameOne = createOption.textContent = currency[0];
-                let valueOne = createOption.value = currency[1].value;
-                currencyOneList.appendChild(createOption);
-                // console.log("option value one is ", currencyNameOne, valueOne);
-                // console.log("optionOne", optionOne);
+                createOption.textContent = currency[0];
 
-                currencyOneList.addEventListener("change", (currency) =>{
-                    let currencyOneTargetValue = currency.target.value;
-                    currencyValueOne = currencyOneTargetValue;
-                    console.log("currencyclicked",currencyOneTargetValue);
-                } )
+                createOption.dataset.code = currency[1].code;
+                createOption.value = currency[1].value;
+
+                let codeOne = currency[1].code;
+                let valueOne = currency[1].value;
+                currencyNameOne = codeOne;
+                console.log(codeOne, "code one is")
+
+                // let item = option.dataset.item;
+                // console.log("option is", option);
+                //That is incorrect i need to grab the dataset correctly, once im able to get the dataset of each individual item, I'll be able to continue
+
+                currencyOneList.appendChild(createOption);
             })
 
             currencyTwoOptions.forEach(() => {
                 let createOption = document.createElement("option");
-                let currencyNameTwo = createOption.textContent = currency[0];
-                currencyValueTwo = currencyNameTwo;
-                let valueTwo = createOption.value = currency[1].value;
+                createOption.textContent = currency[0];
+
+                createOption.dataset.code = currency[1].code;
+                createOption.value = currency[1].value;
+
+                let codeTwo = currency[1].code;
+                let valueTwo = currency[1].value;
+                currencyNameTwo = codeTwo;
+
                 currencyTwoList.appendChild(createOption);
-                // console.log("option value two is", currencyNameTwo, valueTwo);
             })
+        })
 
-            //I'll need first currency value and compare it to usd
-            //then take that usd value and transform it into the second currency by multiplying it
-            const currencyConvertion = () => {
-                //first selected currency + repeat the same for value two
-                console.log("in conversion value one is", currencyValueOne);
-                
-                //transform first currency in usd
-                // that amount of usd multiplied by value to get Second Selected currency
-
-            }
+        currencyOneList.addEventListener("change", (event) => {
+            let currencyOneTargetValue = event.target.value;
+            currencyValueOne = currencyOneTargetValue;
             currencyConvertion();
         })
+
+        currencyTwoList.addEventListener("change", (event) => {
+            let currencyTwoTargetValue = event.target.value;
+            currencyValueTwo = currencyTwoTargetValue;
+            currencyConvertion();
+        })
+
+        const currencyConvertion = () => {
+            //first selected currency + repeat the same for value two
+            console.log("in conversion value one is", currencyValueOne);
+            if (currencyValueTwo !== "ZWL") { //Why after selecting value one does value two becomed ZWL, instead of just staying at 0;
+                console.log("in conversion value two is", currencyValueTwo);
+
+                //Amount logs after currency is selected
+                amount.addEventListener("input", (event) => {
+                    console.log("amount is", event.target.value, event.target.dataset.item); //its not fully working, it always gives me the last element code being ZWL
+                })
+                //amount in input in its own currency 
+                //amount in input is equal to currencyValueOne
+                //amount in input in currencyValueOne needs to be multiplied to become USD
+                //usd amount should be transformed into currencyValueTwo
+                //Show Results
+            }
+            //transform first currency in usd
+            // that amount of usd multiplied by value to get Second Selected currency
+
+        }
+
     })
     .catch(error => console.error('Error fetching data:', error));
 
